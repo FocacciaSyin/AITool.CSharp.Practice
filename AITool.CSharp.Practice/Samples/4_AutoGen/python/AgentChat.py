66b029884ab6aa5257727af5a041142012d3afdc
Woody.Chiu <simoncat524@hotmail.com>
2025-10-10 12:42:59 +0800

    feat: 新增 AgentChat 模組及更新範例程式以整合 OpenAI API

---
+++ AITool.CSharp.Practice/Samples/4_AutoGen/python/AgentChat.py
@@ -0,0 +1,12 @@
+﻿from autogen_agentchat.agents import AssistantAgent
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+
+async def AgentChat(api_key) -> None:
+    model_client = OpenAIChatCompletionClient(
+        model="gpt-4o-mini",
+        api_key=api_key
+    )
+    agent = AssistantAgent("assistant", model_client=model_client)
+    print(await agent.run(task="AutoGen 是甚麼東西??,使用繁體中文回答"))
+    await model_client.close()
--- AITool.CSharp.Practice/Samples/4_AutoGen/python/sample_4_0_autogen_chat.py
+++ AITool.CSharp.Practice/Samples/4_AutoGen/python/sample_4_0_autogen_chat.py
@@ -1,26 +1,21 @@
 ﻿import asyncio
 import os
-from autogen_agentchat.agents import AssistantAgent
-from autogen_ext.models.openai import OpenAIChatCompletionClient
+from dotenv import load_dotenv
+from AgentChat import AgentChat
+
 
 async def main() -> None:
+    # 載入 .env 檔案中的環境變數
+    load_dotenv(encoding='utf-8-sig')
+
     # 從環境變數讀取 API Key
-    # api_key = os.getenv("OPENAI_API_KEY")
-    
-    api_key = "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"
-    
+    api_key = os.getenv("OPENAI_API_KEY")
+
     if not api_key:
-        raise ValueError(
-            "請設定 OPENAI_API_KEY 環境變數於 .env !"
-        )
-    
-    model_client = OpenAIChatCompletionClient(
-        model="gpt-4o-mini",
-        api_key=api_key
-    )
-    agent = AssistantAgent("assistant", model_client=model_client)
-    print(await agent.run(task="AutoGen 是甚麼東西??,使用繁體中文回答"))
-    await model_client.close()
+        raise ValueError("請設定 OPENAI_API_KEY 環境變數於 .env 檔案中!")
+
+    await AgentChat(api_key)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
